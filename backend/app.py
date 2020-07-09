03dcad93fdc1e184ab8a83f469311466d79aac83
Gillian Wong <gillianwong@Gillians-MacBook-Pro.local>
2020-07-09 19:59:32 -0400

    fix indentation

--- backend/app.py
+++ backend/app.py
@@ -609,14 +609,14 @@ def create_app(test_config=None):
         }), AuthError.status_code
 
     if not app.debug:
-    file_handler = FileHandler('error.log')
-    file_handler.setFormatter(
-        Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]')
-    )
-    app.logger.setLevel(logging.INFO)
-    file_handler.setLevel(logging.INFO)
-    app.logger.addHandler(file_handler)
-    app.logger.info('errors')
+        file_handler = FileHandler('error.log')
+        file_handler.setFormatter(
+            Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]')
+        )
+        app.logger.setLevel(logging.INFO)
+        file_handler.setLevel(logging.INFO)
+        app.logger.addHandler(file_handler)
+        app.logger.info('errors')
 
     return app
 
